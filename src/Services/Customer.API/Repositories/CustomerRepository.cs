d12b72204dde937995c4df3addb3139969f1dc65
congtc <congtc@rikkeisoft.com>
2023-04-03 17:21:40 +0700

    5% 3.3

---
+++ src/Services/Customer.API/Repositories/CustomerRepository.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.Common.Interfaces;
+using Customer.API.Persistence;
+using Customer.API.Repositories.Interfaces;
+using Infrastructure.Common;
+using System.Linq.Expressions;
+
+namespace Customer.API.Repositories
+{
+  public class CustomerRepository : RepositoryBaseAsync<Entities.Customer, int, CustomerContext>, ICustomerRepository
+  {
+    public CustomerRepository(CustomerContext dbContext, IUnitOfWork<CustomerContext> unitOfWork) : base(dbContext, unitOfWork)
+    {
+    }
+
+    public Task CreateCustomerAsync(Entities.Customer customer)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task DeleteCustomerAsync(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Entities.Customer> GetCustomerByEmailAsync(string email)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Entities.Customer> GetCustomerByIdAsync(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Entities.Customer> GetCustomerByUserNameAsync(string email)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Entities.Customer>> GetCustomersAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task UpdateCustomerAsync(Entities.Customer customer)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ src/Services/Customer.API/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Common.Interfaces;
+using Customer.API.Persistence;
+
+namespace Customer.API.Repositories.Interfaces
+{
+  public interface ICustomerRepository : IRepositoryQueryBase<Entities.Customer, int, CustomerContext>
+  {
+    Task<IEnumerable<Entities.Customer>> GetCustomersAsync();
+    Task<Entities.Customer> GetCustomerByIdAsync(long id);
+    Task<Entities.Customer> GetCustomerByEmailAsync(string email);
+    Task<Entities.Customer> GetCustomerByUserNameAsync(string email);
+    Task CreateCustomerAsync(Entities.Customer customer);
+    Task UpdateCustomerAsync(Entities.Customer customer);
+    Task DeleteCustomerAsync(long id);
+  }
+}
